0c3b14c6f778efd0702b98333aea53c955269d7f
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-27 01:04:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+require 'pry'
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator (attendees)
+  attendees.map do |person|
+    binding.pry
+    "Hello, my name is #{person}"
+  end
+end 
